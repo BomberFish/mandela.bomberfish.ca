6bdb85010f35a7db20116d2b364de4e6c2d81f34
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-22 19:19:06 -0500

    omg javascript?!

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+function showDownload() {
+  document.querySelector("#dlscreen").className = "show";
+}
+
+function hideDownload() {
+  document.querySelector("#dlscreen").className = "";
+}
