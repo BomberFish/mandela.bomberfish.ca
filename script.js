b8b72913599bb29d74dcfd4b98223dc15dffd45e
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-22 19:22:34 -0500

    Richard Stallman moment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 console.log("Hello, World!");
 
 function showDownload() {
@@ -7,3 +8,4 @@ function showDownload() {
 function hideDownload() {
   document.querySelector("#dlscreen").className = "";
 }
+// @license-end
