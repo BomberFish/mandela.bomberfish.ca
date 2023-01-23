3a5aa415a2f9f779884036f6583d4cce20af92ce
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-22 19:24:32 -0500

    Fix an edge casE?

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 console.log("Hello, World!");
 
+document.querySelector("#dlscreen").className = "";
+
 function showDownload() {
   document.querySelector("#dlscreen").className = "show";
 }
